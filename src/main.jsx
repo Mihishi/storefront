6e4c5e5a32aca188bdf1dc2720bd948cd10a77ee
Mihishi <mihishitharushika@gmail.com>
2025-02-21 13:55:16 +0530

    Week-8

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+
+
+import { BrowserRouter,Routes,Route } from 'react-router'
+import HomePage from "./pages/home.page.jsx"
+import SignInPage from "./pages/sign-in.page";
+import SignUpPage from "./pages/sign-up.page";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={ <HomePage />}/>
+    <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
